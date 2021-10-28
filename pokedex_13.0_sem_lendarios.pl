96fbf1b49879c08b61b716bbe2154d89dea5f682
Allan Bismarck de Souza Santos <42256201+AllanBismarck123@users.noreply.github.com>
2021-10-27 21:19:34 -0300

    Create pokedex_13.0_sem_lendarios.pl

---
+++ pokedex_13.0_sem_lendarios.pl
@@ -0,0 +1,363 @@
+pokemon(bulbasaur,grama,sim).
+pokemon(ivysaur,grama,sim).
+pokemon(venusaur,grama,nao).
+pokemon(charmander,fogo,sim).
+pokemon(charmeleon,fogo,sim).
+pokemon(charizard,fogo,nao).
+pokemon(squirtle,agua,sim).
+pokemon(wartortle,agua,sim).
+pokemon(blastoise,agua,nao).
+pokemon(caterpie,inseto,sim).
+pokemon(metapod,inseto,sim).
+pokemon(butterfree,inseto,nao).
+pokemon(weedle,inseto,sim).
+pokemon(kakuna,inseto,sim).
+pokemon(beedrill,inseto,nao).
+pokemon(pidgey,normal,sim).
+pokemon(pidgeotto,normal,sim).
+pokemon(pidgeot,normal,nao).
+pokemon(rattata,normal,sim).
+pokemon(raticate,normal,nao).
+pokemon(spearow,normal,sim).
+pokemon(fearow,normal,nao).
+pokemon(ekans,veneno,sim).
+pokemon(arbok,veneno,nao).
+pokemon(pikachu,eletrico,sim).
+pokemon(raichu,eletrico,nao).
+pokemon(sandshrew,terra,sim).
+pokemon(sandslash,terra,nao).
+pokemon(nidoran_femea,veneno,sim).
+pokemon(nidorina,veneno,sim).
+pokemon(nidoqueen,veneno,nao).
+pokemon(nidoran_macho,veneno,sim).
+pokemon(nidorino,veneno,sim).
+pokemon(nidoking,veneno,nao).
+pokemon(clefairy,fada,sim).
+pokemon(clefable,fada,nao).
+pokemon(vulpix,fogo,sim).
+pokemon(ninetales,fogo,nao).
+pokemon(jigglypuff,fada,sim).
+pokemon(wigglytuff,fada,nao).
+pokemon(zubat,veneno,sim).
+pokemon(golbat,veneno,nao).
+pokemon(oddish,grama,sim).
+pokemon(gloom,grama,sim).
+pokemon(vileplume,grama,nao).
+pokemon(paras,inseto,sim).
+pokemon(parasect,inseto,nao).
+pokemon(venonat,inseto,sim).
+pokemon(venomoth,inseto,nao).
+pokemon(diglett,terra,sim).
+pokemon(dugtrio,terra,nao).
+pokemon(meowth,normal,sim).
+pokemon(persian,normal,nao).
+pokemon(psyduck,agua,sim).
+pokemon(golduck,agua,nao).
+pokemon(mankey,lutador,sim).
+pokemon(primeape,lutador,nao).
+pokemon(growlithe,fogo,sim).
+pokemon(arcanine,fogo,nao).
+pokemon(poliwag,agua,sim).
+pokemon(poliwhirl,agua,sim).
+pokemon(poliwrath,agua,nao).
+pokemon(abra,psiquico,sim).
+pokemon(kadabra,psiquico,sim).
+pokemon(alakazam,psiquico,nao).
+pokemon(machop,lutador,sim).
+pokemon(machoke,lutador,sim).
+pokemon(machamp,lutador,nao).
+pokemon(bellsprout,grama,sim).
+pokemon(weepinbell,grama,sim).
+pokemon(victreebel,grama,nao).
+pokemon(tentacool,agua,sim).
+pokemon(tentacruel,agua,nao).
+pokemon(geodude,pedra,sim).
+pokemon(graveler,pedra,sim).
+pokemon(golem,pedra,nao).
+pokemon(ponyta,fogo,sim).
+pokemon(rapidash,fogo,nao).
+pokemon(slowpoke,agua,sim).
+pokemon(slowbro,agua,nao).
+pokemon(magnemite,eletrico,sim).
+pokemon(magneton,eletrico,nao).
+pokemon(farfetch_d,normal,sim).
+pokemon(doduo,normal,sim).
+pokemon(dodrio,normal,nao).
+pokemon(seel,agua,sim).
+pokemon(dewgong,agua,nao).
+pokemon(grimer,veneno,sim).
+pokemon(muk,veneno,nao).
+pokemon(shellder,agua,sim).
+pokemon(cloyster,agua,nao).
+pokemon(gastly,fantasma,sim).
+pokemon(haunter,fantasma,sim).
+pokemon(gengar,fantasma,nao).
+pokemon(onix,pedra,nao).
+pokemon(drowzee,psiquico,sim).
+pokemon(hypno,psiquico,nao).
+pokemon(krabby,agua,sim).
+pokemon(kingler,agua,nao).
+pokemon(voltorb,eletrico,sim).
+pokemon(electrode,eletrico,nao).
+pokemon(exeggcute,grama,sim).
+pokemon(exeggutor,grama,nao).
+pokemon(cubone,terra,sim).
+pokemon(marowak,terra,nao).
+pokemon(hitmonlee,lutador,sim).
+pokemon(hitmonchan,lutador,nao).
+pokemon(lickitung,normal,nao).
+pokemon(koffing,veneno,sim).
+pokemon(weezing,veneno,nao).
+pokemon(rhyhorn,terra,sim).
+pokemon(rhydon,terra,nao).
+pokemon(chansey,normal,nao).
+pokemon(tangela,grama,nao).
+pokemon(kangaskhan,normal,nao).
+pokemon(horsea,agua,sim).
+pokemon(seadra,agua,nao).
+pokemon(goldeen,agua,sim).
+pokemon(seaking,agua,nao).
+pokemon(staryu,agua,sim).
+pokemon(starmie,agua,nao).
+pokemon(mr_mime,psiquico,nao).
+pokemon(scyther,inseto,nao).
+pokemon(jynx,gelo,nao).
+pokemon(electabuzz,eletrico,nao).
+pokemon(magmar,fogo,nao).
+pokemon(pinsir,inseto,nao).
+pokemon(tauros,normal,nao).
+pokemon(magikarp,agua,sim).
+pokemon(gyarados,agua,nao).
+pokemon(lapras,agua,nao).
+pokemon(ditto,normal,nao).
+pokemon(eevee,normal,sim).
+pokemon(vaporeon,agua,nao).
+pokemon(jolteon,eletrico,nao).
+pokemon(flareon,fogo,nao).
+pokemon(porygon,normal,nao).
+pokemon(omanyte,pedra,sim).
+pokemon(omastar,pedra,nao).
+pokemon(kabuto,pedra,sim).
+pokemon(kabutops,pedra,nao).
+pokemon(aerodactyl,pedra,nao).
+pokemon(snorlax,normal,nao).
+pokemon(articuno,gelo,nao).
+pokemon(zapdos,eletrico,nao).
+pokemon(moltres,fogo,nao).
+pokemon(dratini,dragao,sim).
+pokemon(dragonair,dragao,sim).
+pokemon(dragonite,dragao,nao).
+pokemon(mewtwo,psiquico,nao).
+pokemon(mew,psiquico,nao).
+
+vantagem(veneno,fada).
+vantagem(metal,fada).
+vantagem(fogo,metal).
+vantagem(lutador,metal).
+vantagem(terra,metal).
+vantagem(lutador,noturno).
+vantagem(inseto,noturno).
+vantagem(fada,noturno).
+vantagem(gelo,dragao).
+vantagem(dragao,dragao).
+vantagem(fada,dragao).
+vantagem(fantasma,fantasma).
+vantagem(noturno,fantasma).
+vantagem(agua,pedra).
+vantagem(grama,pedra).
+vantagem(lutador,pedra).
+vantagem(terra,pedra).
+vantagem(metal,pedra).
+vantagem(fogo,inseto).
+vantagem(voador,inseto).
+vantagem(pedra,inseto).
+vantagem(inseto,psiquico).
+vantagem(fantasma,psiquico).
+vantagem(noturno,psiquico).
+vantagem(eletrico,voador).
+vantagem(gelo,voador).
+vantagem(pedra,voador).
+vantagem(agua,terra).
+vantagem(grama,terra).
+vantagem(gelo,terra).
+vantagem(terra,veneno).
+vantagem(psiquico,veneno).
+vantagem(voador,lutador).
+vantagem(psiquico,lutador).
+vantagem(fada,lutador).
+vantagem(fogo,gelo).
+vantagem(lutador,gelo).
+vantagem(pedra,gelo).
+vantagem(metal,gelo).
+vantagem(fogo,grama).
+vantagem(gelo,grama).
+vantagem(veneno,grama).
+vantagem(voador,grama).
+vantagem(inseto,grama).
+vantagem(terra,eletrico).
+vantagem(eletrico,agua).
+vantagem(grama,agua).
+vantagem(agua,fogo).
+vantagem(terra,fogo).
+vantagem(pedra,fogo).
+vantagem(lutador,normal).
+
+evolui(bulbasaur,ivysaur).
+evolui(ivysaur, venusaur).
+evolui(charmander,charmeleon).
+evolui(charmeleon,charizard).
+evolui(squirtle,wartortle).
+evolui(wartortle,blastoise).
+evolui(caterpie,metapod).
+evolui(metapod,butterfree).
+evolui(weedle,kakuna).
+evolui(kakuna,beedrill).
+evolui(pidgey,pidgeotto).
+evolui(pidgeotto,pidgeot).
+evolui(rattata,raticate).
+evolui(spearow,fearow).
+evolui(ekans,arbok).
+evolui(pikachu,raichu).
+evolui(sandshrew,sandslash).
+evolui(nidoran_femea,nidorina).
+evolui(nidorina,nidoqueen).
+evolui(nidoran_macho,nidorino).
+evolui(nidorino,nidoking).
+evolui(clefairy,clefable).
+evolui(vulpix,ninetales).
+evolui(jigglypuff,wigglytuff).
+evolui(zubat,golbat).
+evolui(oddish,gloom).
+evolui(vileplume,paras).
+evolui(paras,parasect).
+evolui(venonat,venomoth).
+evolui(diglett,dugtrio).
+evolui(meowth,persian).
+evolui(psyduck,golduck).
+evolui(mankey,primeape).
+evolui(growlithe,arcanine).
+evolui(poliwag,poliwhirl).
+evolui(poliwhirl,poliwrath).
+evolui(abra,kadabra).
+evolui(kadabra,alakazam).
+evolui(machop,machoke).
+evolui(machoke,machamp).
+evolui(bellsprout,weepinbell).
+evolui(weepinbell,victreebel).
+evolui(tentacool,tentacruel).
+evolui(geodude,graveler).
+evolui(graveler,golem).
+evolui(ponyta,rapidash).
+evolui(slowpoke,slowbro).
+evolui(magnemite,magneton).
+%evolui(Farfetch'd,NÂO TEM EVOLUÇÃO).
+evolui(doduo,dodrio).
+evolui(seel,dewgong).
+evolui(grimer,muk).
+evolui(shellder,cloyster).
+evolui(gastly,haunter).
+evolui(haunter,gengar).
+%evolui(onix,NÃO TEM EVOLUÇÃO).
+evolui(drowzee,hypno).
+evolui(krabby,kingler).
+evolui(voltorb,electrode).
+evolui(exeggcute,exeggutor).
+evolui(cubone,marowak).
+evolui(hitmonlee,hitmonchan).
+%evolui(lickitung,NÃO TEM EVOLUÇÃO).
+evolui(koffing,weezing).
+evolui(rhyhorn,rhydon).
+%evolui(chansey,NÃO TEM EVOLUÇÃO).
+%evolui(tangela,NÃO TEM EVOLUÇÃO).
+%evolui(kangaskhan,NÃO TEM EVOLUÇÃO).
+evolui(horsea,seadra).
+evolui(goldeen,seaking).
+evolui(staryu,starmie).
+%evolui(mr_mime,NÃO TEM EVOLUÇÃO).
+%evolui(scyther,NÃO TEM EVOLUÇÃO).
+%evolui(jynx,NÃO TEM EVOLUÇÃO).
+%evolui(electabuzz,NÃO TEM EVOLUÇÃO).
+%evolui(magmar,NÃO TEM EVOLUÇÃO).
+%evolui(pinsir,NÃO TEM EVOLUÇÃO).
+%evolui(tauros,NÃO TEM EVOLUÇÃO).
+evolui(magikarp,gyarados).
+%evolui(lapras,NÃO TEM EVOLUÇÃO).
+%evolui(ditto,NÃO TEM EVOLUÇÃO).
+evolui(eevee,vaporeon).
+evolui(eevee,jolteon).
+evolui(eevee,flareon).
+%evolui(porygon,NÃO TEM EVOLUÇÃO).
+evolui(omanyte,omastar).
+evolui(kabuto,kabutops).
+%evolui(aerodactyl,NÃO TEM EVOLUÇÃO).
+%evolui(snorlax,NÃO TEM EVOLUÇÃO).
+%evolui(articuno,NÃO TEM EVOLUÇÃO).
+%evolui(zapdos,NÂO TEM EVOLUÇÃO).
+%evolui(moltres,NÃO TEM EVOLUÇÃO).
+evolui(dratini,dragonair).
+evolui(dragonair,dragonite).
+%evolui(mewtwo,NÃO TEM EVOLUÇÃO).
+%evolui(mew,NÃO TEM EVOLUÇÃO).
+
+lendario(mew).
+lendario(mewtwo).
+lendario(articuno).
+lendario(zapdos).
+lendario(moltres).
+
+%recursividade na busca por pokémons da mesma linhagem de evolução.
+caminho(X,Y):- evolui(X,Y).
+caminho(X,Y):- evolui(X,Z),caminho(Z,Y).
+
+sem_evolucao(X):-not(evolui(X,_)),not(evolui(_,X)),not(lendario(X)). %não possui evolução e não é lendario. É o mais fraco
+primario(X):-not(evolui(_,X)),evolui(X,Y),evolui(Y,_). %possui evolução e essa evolução também possui evolução. ex:bulbasaur.
+primario(X):-not(evolui(_,X)),evolui(X,Y),not(evolui(Y,_)). %caso em que ele só tenha uma evolução, considerei como primário.ex:pikachu, rattata, etc.
+intermedio(X):-evolui(_,X),evolui(X,_). %é evolução de um pokémon e tem evolução. ex:charmeleon.
+final(X):-evolui(_,X),not(evolui(X,_)),not(lendario(X)). %forma final, é evolução de um pkm e tem evolução
+final(X):-pokemon(X,_,nao),lendario(X). %forma final, são lendarios.
+
+experiencia(X,Y):-final(X),intermedio(Y).
+experiencia(X,Y):-final(X),primario(Y).
+experiencia(X,Y):-final(X),sem_evolucao(Y).
+experiencia(X,Y):-intermedio(X),primario(Y).
+experiencia(X,Y):-intermedio(X),sem_evolucao(Y).
+
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),final(X),final(Y). %os dois são do mesmo tipo e são final
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),intermedio(X),intermedio(Y). %os dois são do mesmo tipo e são intermedios.
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),primario(X),primario(Y). %os dois são do mesmo tipo e são primários.
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),sem_evolucao(X),sem_evolucao(Y).
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),sem_evolucao(X),primario(Y).
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),primario(X),sem_evolucao(Y).
+empate(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),not(experiencia(X,Y)),not(experiencia(Y,X)),A\=B.
+
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(A,B),B\='dragao',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(A,B),A\='dragao',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(A,B),B\='fantasma',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(A,B),A\='fantasma',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(X,Y),B\='dragao'.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(X,Y),A\='dragao',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(X,Y),B\='fantasma',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(X,Y),A\='fantasma',A\=B.
+vitoria(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),experiencia(X,Y). %se são do mesmo tipo e se X tem mais experiência do que Y.
+
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(B,A),B\='dragao',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(B,A),A\='dragao',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(B,A),B\='fantasma',A=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),vantagem(B,A),A\='fantasma',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(Y,X),B\='dragao',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(Y,X),A\='dragao',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(Y,X),B\='fantasma',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,B,_),not(vantagem(A,B)),not(vantagem(B,A)),experiencia(Y,X),A\='fantasma',A\=B.
+derrota(X,Y):-pokemon(X,A,_),pokemon(Y,A,_),experiencia(Y,X). %se são do mesmo tipo e se Y tem mais experiência do que X
+
+batalha(X,Y,Z):-empate(X,Y)->Z =empate. %se empate, Z recebe a string "empate".
+batalha(X,Y,Z):-vitoria(X,Y)->string_concat('vitoria de ',X,Z). %se vitoria de X sobre Y, Z recebe a string "vitoria de <X>".
+batalha(X,Y,Z):-derrota(X,Y)->string_concat('vitoria de ',Y,Z). %se vitoria de Y sobre X, Z recebe a string "vitoria de <Y>".
+
+
+
+
+
+
+
+
